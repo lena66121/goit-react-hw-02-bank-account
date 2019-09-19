236721f07ef45c5c1c96a15aac1f0496540ecf64
Lena <2863040@ukr.net>
2019-09-20 00:25:12 +0300

    Updates

---
+++ precache-manifest.7607c1a313fe811c9e20293708fddf29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f58ec326a463dbac5103d20dae7967",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "f43cbcb00441ea76bc5f",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "b4109b957fc3223bfaff",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.a997da65.chunk.css"
+  },
+  {
+    "revision": "f43cbcb00441ea76bc5f",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.b89c0b94.chunk.js"
+  },
+  {
+    "revision": "b4109b957fc3223bfaff",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.0fdf788b.chunk.js"
+  },
+  {
+    "revision": "bfe9bfa919ee6a7c9d8c",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime~main.cd27391c.js"
+  }
+]);
